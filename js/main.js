2aa472606a426e1b85554604fd901fdbc6048000
Татьяна <ltasya72@gmail.com>
2023-08-24 01:14:10 +0300

    header-centr__card

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // !Спойлер
 $(document).ready(function () {
   $(".header-center__phone-arrow").click(function (event) {
-    $(this).toggleClass("active").next().slideToggle(300);
+      $(this).toggleClass("active").next().slideToggle(300);
   });
 });
