41785cb9fd9c4eed34b68d9706aa125442acb23f
Татьяна <ltasya72@gmail.com>
2023-08-26 14:20:48 +0300

    вёрстка header-catalog с адаптивом

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
 // !Спойлер
 $(document).ready(function () {
   $(".header-center__phone-arrow").click(function (event) {
-      $(this).toggleClass("active").next().slideToggle(300);
+    $(this).toggleClass("active").next().slideToggle(300);
   });
 });
+
+document.addEventListener("click", documentActions);
+function documentActions(e) {
+  const targetElement = e.target;
+  if (targetElement.closest("[data-parent]")) {
+    const subMenuId = targetElement.dataset.parent
+      ? targetElement.dataset.parent
+      : null;
+    const subMenu = document.querySelector(`[data-submenu="${subMenuId}"]`);
+    const catalogMenu = document.querySelector(".header-catalog__submenu");
+
+    if (subMenu) {
+      const activeLink = document.querySelector("._submenu-active");
+      const activeBlock = document.querySelector("._submenu-open");
+
+      if (activeLink && activeLink !== targetElement) {
+        activeLink.classList.remove("_submenu-active");
+        activeBlock.classList.remove("_submenu-open");
+      }
+      targetElement.classList.toggle("_submenu-active");
+      subMenu.classList.toggle("_submenu-open");
+
+    } else {
+      console.log("Нет такого подменю :('");
+    }
+
+    e.preventDefault();
+  }
+}
