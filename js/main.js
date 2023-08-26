efb09bade2b2f9156c9e0a6604e04a551e70e9f0
Татьяна <ltasya72@gmail.com>
2023-08-26 20:50:50 +0300

    header свёрстан

--- js/main.js
+++ js/main.js
@@ -6,30 +6,41 @@ $(document).ready(function () {
 });
 
 document.addEventListener("click", documentActions);
-function documentActions(e) {
-  const targetElement = e.target;
-  if (targetElement.closest("[data-parent]")) {
-    const subMenuId = targetElement.dataset.parent
-      ? targetElement.dataset.parent
-      : null;
-    const subMenu = document.querySelector(`[data-submenu="${subMenuId}"]`);
-    const catalogMenu = document.querySelector(".header-catalog__submenu");
-
-    if (subMenu) {
-      const activeLink = document.querySelector("._submenu-active");
-      const activeBlock = document.querySelector("._submenu-open");
-
-      if (activeLink && activeLink !== targetElement) {
-        activeLink.classList.remove("_submenu-active");
-        activeBlock.classList.remove("_submenu-open");
-      }
-      targetElement.classList.toggle("_submenu-active");
-      subMenu.classList.toggle("_submenu-open");
-
-    } else {
-      console.log("Нет такого подменю :('");
+const manuBlocks = document.querySelectorAll(".header-catalog__submenu-block");
+if (manuBlocks.length) {
+  manuBlocks.forEach(menuBlock => {
+        const manuBlockItems = menuBlock.querySelectorAll(
+          ".header-catalog__submenu-category").length;
+    menuBlock.classList.add(`header-catalog__submenu-block--${manuBlockItems}`);
+      });
     }
+      function documentActions(e) {
+        const targetElement = e.target;
+        if (targetElement.closest("[data-parent]")) {
+          const subMenuId = targetElement.dataset.parent
+            ? targetElement.dataset.parent
+            : null;
+          const subMenu = document.querySelector(
+            `[data-submenu="${subMenuId}"]`
+          );
+          const catalogMenu = document.querySelector(
+            ".header-catalog__submenu"
+          );
 
-    e.preventDefault();
-  }
-}
+          if (subMenu) {
+            const activeLink = document.querySelector("._submenu-active");
+            const activeBlock = document.querySelector("._submenu-open");
+
+            if (activeLink && activeLink !== targetElement) {
+              activeLink.classList.remove("_submenu-active");
+              activeBlock.classList.remove("_submenu-open");
+            }
+            targetElement.classList.toggle("_submenu-active");
+            subMenu.classList.toggle("_submenu-open");
+          } else {
+            console.log("Нет такого подменю :('");
+          }
+
+          e.preventDefault();
+        }
+      }
