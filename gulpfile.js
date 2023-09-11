c83835c1648fa2130eea36837db469723bde3a0e
Татьяна <ltasya72@gmail.com>
2023-09-11 17:52:02 +0300

    начата работа над _catalog

--- gulpfile.js
+++ gulpfile.js
@@ -75,20 +75,20 @@ function scripts() {
 function images() {
   // return src(["app/images/**/*.*", "!app/images/**/*.svg"])
   return (
-    src(["app/images/src/*.*", "!app/images/src/*.svg"])
+    src(["images/src/catalog-cards/*.*", "!images/src/*.svg"])
       // .pipe(newer("app/images"))
       // .pipe(avif({ quality: 50 }))
 
       // .pipe(src("app/images/**/*.*"))
-      .pipe(src("app/images/src/*.*"))
-      .pipe(newer("app/images"))
+      .pipe(src("images/src/catalog-cards/*.*"))
+      .pipe(newer("images"))
       .pipe(webp())
 
-      .pipe(src("app/images/src/*.*"))
-      .pipe(newer("app/images"))
+      .pipe(src("images/src/catalog-cards/*.*"))
+      .pipe(newer("images"))
       .pipe(imagemin())
 
-      .pipe(dest("app/images"))
+      .pipe(dest("images/catalog-cards/"))
   );
 }
 
